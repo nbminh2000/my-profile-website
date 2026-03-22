0589ccca7ffd73ecbbf3d4082324b82dfeefbf99
Nguyen Binh Minh <binhminhvd212@gmail.com>
2026-03-22 21:10:23 +0700

    msg

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
     base: command === 'build' ? '/my-profile-website/' : '/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
   }
-});
+})
